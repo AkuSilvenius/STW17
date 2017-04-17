fa3830ebdb26d28b76de8cbc2996d07820d2480f
Jannkar <janne.a.karttunen@gmail.com>
2017-04-17 16:22:27 +0300

    Add files via upload

--- parsedata.py
+++ parsedata.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import print_function
 from lxml import etree
 import xml.etree.ElementTree as ET
+import io
 
 parser = etree.XMLParser(recover=True)
 
 tree = ET.parse('./data/MovieDiC_V2.xml', parser=parser)
 root = tree.getroot()
+file = io.open('cleanedData.txt','w',encoding='utf8')
 
 i = tree.getiterator()
 
+print('Cleanin up the data...')
 for m in i:
-    print(m.tag)
+    #print(m.tag)
     for di in m.findall('dialogue'):
-        print("dialogue id: " + di.get('id'))
-        for u in di.findall('speaker'):
-            print(u.text + ": " + u.get('utterance'))
-        print("dialogue changes")
+        #print("dialogue id: " + di.get('id'))
+        for u in di.findall('utterance'):
+            utterance = u.text
+            file.write(u.text + "\n")
+    #print("dialogue changes")
+file.close()
+print('Clean up complete!')
